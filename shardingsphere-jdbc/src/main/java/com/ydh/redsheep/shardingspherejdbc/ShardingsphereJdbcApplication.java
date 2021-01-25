eff5bdd8af30bb9da4e218cf795d50adc586e24d
yangdehong <405104787@qq.com>
2021-01-25 18:23:45 +0800

    分布式事务xa

--- shardingsphere-jdbc/src/main/java/com/ydh/redsheep/shardingspherejdbc/ShardingsphereJdbcApplication.java
+++ shardingsphere-jdbc/src/main/java/com/ydh/redsheep/shardingspherejdbc/ShardingsphereJdbcApplication.java
@@ -2,10 +2,11 @@ package com.ydh.redsheep.shardingspherejdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@SpringBootApplication
+@SpringBootApplication(exclude = JtaAutoConfiguration.class)
 public class ShardingsphereJdbcApplication {
 
     public static void main(String[] args) {
