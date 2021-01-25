9bb08ec5aa5d7b7d8618dd5046f68cf8562e5966
yangdehong <405104787@qq.com>
2021-01-25 09:26:28 +0800

    proxy

---
+++ shardingsphere-proxy/src/main/java/com/ydh/redsheep/shardingsphereproxy/ShardingsphereProxyApplication.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.shardingsphereproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingsphereProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingsphereProxyApplication.class, args);
+    }
+
+}
---
+++ shardingsphere-proxy/src/test/java/com/ydh/redsheep/shardingsphereproxy/ShardingsphereProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.shardingsphereproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
